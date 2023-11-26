63c12229c9bb3b8c49facb5c7e93daa81270841c
Francesco Mari <francescojjmari@gmail.com>
2023-11-27 00:07:17 +0100

    Update sanity integration config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 import { defineConfig } from 'astro/config';
-
-import sanity from "@sanity/astro";
+import { sanityIntegration as sanity } from "@sanity/astro";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sanity()]
+  output: 'static',
+  // adapter: vercel(),
+  integrations: [sanity({
+    projectId: '45a8gqkh',
+    dataset: 'production',
+    apiVersion: '2021-03-25',
+    useCdn: false,
+  }), react()]
 });
